625ad50fa611dd5e8c04acaf7b577c86879cfec5
Barham Farraj <barhamfarraj@icloud.com>
2022-04-11 02:28:41 +0300

    init3

--- serverless/Hi.py
+++ serverless/Hi.py
@@ -1,13 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    #  request is successful and you will get your response
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = "Hello Amman python-401d8"
-    self.wfile.write(message.encode())
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
