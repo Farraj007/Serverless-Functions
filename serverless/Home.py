a5c57a2963b61a391525d58857a86ca539115d14
Barham Farraj <barhamfarraj@icloud.com>
2022-04-11 00:41:54 +0300

    init2

--- serverless/Home.py
+++ serverless/Home.py
@@ -7,7 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        message = Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
         return
     
