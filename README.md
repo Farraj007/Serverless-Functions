3ef4d7cf87062c032b03c9d4101cf262d8b3909b
Barham Farraj <barhamfarraj@icloud.com>
2022-04-11 16:46:07 +0300

    finish

---
+++ README.md
@@ -0,0 +1,11 @@
+## To see the serverless function :
+- First click on this [url](https://serverless-functions-sandy.vercel.app/)
+- Then type **/api/Hi** at the end of the url path
+- You will see the calendar of this year, plus the datetime and week number/month.
+
+
+As can be seen in the picture below: 
+
+![calendar](Calendar.jpg)
+
+**I Used the aprsing method Yahiha Mentioned on SLACK**
--- api/Home.py
+++ api/Home.py
@@ -1,13 +1,20 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_Get(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.send_response(200)
-        self.path = ''
-        with open(self.path, 'rb') as f:
-            self.wfile.write(f.read())
-        return
+#     def do_Get(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'text/plain')
+#         self.end_headers()
+#         self.send_response(200)
+#         self.path = ''
+#         with open(self.path, 'rb') as f:
+#             self.wfile.write(f.read())
+#         return
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
