073b5d8c13f5c557b0106ec2925cdc06ae496f61
Barham Farraj <barhamfarraj@icloud.com>
2022-04-11 12:48:46 +0300

    init4

--- api/Hi.py
+++ api/Hi.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
-from urllib.parse import urlparse
 
 class handler(BaseHTTPRequestHandler):
 
@@ -8,6 +7,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    u=urlparse(https://serverless-functions-4xjz2dsnl-farraj007.vercel.app/api/home)
+    message='Today is ' + str(datetime.now())
+    self.wfile.write(str(message.strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
