36aaa4a3a25fd853cfc24099436af18bb846a78e
Barham Farraj <barhamfarraj@icloud.com>
2022-04-11 12:52:52 +0300

    init4

--- api/Hi.py
+++ api/Hi.py
@@ -7,6 +7,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message='Today is ' + str(datetime.now())
-    self.wfile.write(str(message.strftime('%Y-%m-%d %H:%M:%S')).encode())
+    message='Today is ' + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    self.wfile.write(message.encode())
     return
